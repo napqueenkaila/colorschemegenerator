8ed769b17c969f23d4eb071e5d2fd985d654949a
napqueenkaila <125321591+napqueenkaila@users.noreply.github.com>
2023-07-14 18:06:25 -0400

    updates/finishing touches

--- index.js
+++ index.js
@@ -15,14 +15,32 @@ function getColorScheme(){
         .then(data => {
             data.colors.map(color =>{
                 displayColorHtml += `
-                    <div class="color-container">
-                        <div class="color" style="background-color:${color.hex.value}"></div>
+                    <div class="color-container" data-hex="${color.hex.value}">
+                        <div class="color" style="background-color:${color.hex.value}" data-hex="${color.hex.value}"></div>
                         <p class="hex-value copy">${color.hex.value}</p>
                     </div>
                 `
             })
-            console.log(displayColorHtml)
             document.getElementById("color-display").innerHTML = displayColorHtml
         })
 }
+
+// COPY HEX VALUES
+document.getElementById("color-display").addEventListener("click", copyHexValue)
+
+const copiedMessage = document.getElementById("copied-message")
+
+function copyHexValue(e) {
+    navigator.clipboard.writeText(e.target.dataset.hex)
+        .then(() => {
+            copiedMessage.style.display = "block"
+            setTimeout(() => {
+                copiedMessage.style.display = "none"
+            }, 1500)
+        })
+        .catch((err) => {
+            console.error("Failed to copy:", err)
+        })
+}
+
 
